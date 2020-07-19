39798dac2c21dd41c977204a5c2b667ee7454807
Mike <mike.nichols97@gmail.com>
2020-07-19 18:11:57 -0400

    Site title added and review page (mostly) done

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { useParams} from 'react-router-dom';
+import {useParams, Redirect} from 'react-router-dom';
 import request from '../utils/makeRequest';
 import Review from '../utils/Review';
 import handleTitle from '../utils/handleTitle';
@@ -10,68 +10,62 @@ interface ReviewInfoProps {
 }
 
 const ReviewInfo: React.FC<ReviewInfoProps> = ({review}) => {
-    const {rank, movie, total, director, genre, subgenre, universe, subuniverse, character, sportholiday, year, poster} = review;
     return(
+        !review.director? <Redirect push to="/" /> :
         <>
-            <h2>{movie}</h2>
-            <h2>Score: {total} (#{rank})</h2>
-            <img src={poster} />
-            <table id="movieReviews">
-                <thead>
-                    <tr>
-                        <th>Streaming</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>0000</td>
-                        <td>0000</td>
-                        <td>0000</td>
-                        <td>0000</td>
-                    </tr>
-                </tbody>
-            </table>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>Director:</td>
-                        <td>{director}</td>
-                    </tr>
-                    <tr>
-                        <td>Year Released:</td>
-                        <td>{year}</td>
-                    </tr>
-                    <tr>
-                        <td>Genre:</td>
-                        <td>{genre}</td>
-                    </tr>
-                    <tr>
-                        <td>Sub-Genre:</td>
-                        <td>{subgenre || "N/A"}</td>
-                    </tr>
-                    <tr>
-                        <td>Universe:</td>
-                        <td>{universe || "N/A"}</td>
-                    </tr>
-                    <tr>
-                        <td>Sub-Universe:</td>
-                        <td>{subuniverse || "N/A"}</td>
-                    </tr>
-                    <tr>
-                        <td>Character:</td>
-                        <td>{character || "N/A"}</td>
-                    </tr>
-                    <tr>
-                        <td>Sport/Holiday:</td>
-                        <td>{sportholiday || "N/A"}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <h2>{review.movie}</h2>
+            <img src={review.poster} />
+            <div id="review-card">
+                <h2>Score: {review.total} (#{review.rank})</h2>
+                <div id="review-streaming">
+                    <h3>Streaming</h3>
+                    <hr />
+                    <div style={{color: "gray"}}>
+                        {"{{Streaming Options Will Go Here}}"}
+                    </div>
+                </div>
+                <table id="review-info">
+                    <tbody>
+                        <tr>
+                            <td>Director:</td>
+                            <td>{review.director}</td>
+                        </tr>
+                        <tr>
+                            <td>Year Released:</td>
+                            <td>{review.year}</td>
+                        </tr>
+                        <tr>
+                            <td>Genre:</td>
+                            <td>{review.genre}</td>
+                        </tr>
+                        <tr>
+                            <td>Sub-Genre:</td>
+                            <td>{review.subgenre || "N/A"}</td>
+                        </tr>
+                        <tr>
+                            <td>Universe:</td>
+                            <td>{review.universe || "N/A"}</td>
+                        </tr>
+                        <tr>
+                            <td>Sub-Universe:</td>
+                            <td>{review.subuniverse || "N/A"}</td>
+                        </tr>
+                        <tr>
+                            <td>Character:</td>
+                            <td>{review.character || "N/A"}</td>
+                        </tr>
+                        <tr>
+                            <td>Sport/Holiday:</td>
+                            <td>{review.sportholiday || "N/A"}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
 
-const ReviewPage: React.FC = () => {
+const ReviewPage: React.FC = (props) => {
 
     const {rank} = useParams();
     const[loading, setLoading] = useState<boolean>(true);
@@ -83,11 +77,14 @@ const ReviewPage: React.FC = () => {
     useEffect(() => {
         request(`reviews/movie/${rank}`)
         .then((res: any) => {
-            // if(!res.data) alert & back to home here
+            if(!res.data) {
+                alert("No Movie Found!")
+            } else {
+                const {movie} = res.data
+                if(movie.substring(movie.length-5).toLowerCase() === ", the") res.data.movie = handleTitle(movie);
+                setReview(res.data);
+            }
             setLoading(false);
-            const {movie} = res.data
-            if(movie.substring(movie.length-5).toLowerCase() === ", the") res.data.movie = handleTitle(movie);
-            setReview(res.data);
         })
     }, [])
 
