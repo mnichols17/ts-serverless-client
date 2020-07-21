9d3dda7edb8280f3330ceb6fd8bd51c2148913f3
Mike <mike.nichols97@gmail.com>
2020-07-21 14:15:35 -0400

    Adding build folder for Netlify

---
+++ build/precache-manifest.59ccb80f6be03c19e198c1c4c023a109.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52764023b025b8d8f608ab2eed3526e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02fac6b4731bbe33ae2b",
+    "url": "/static/css/main.f1ce26e2.chunk.css"
+  },
+  {
+    "revision": "55d6cd98d49a224582ba",
+    "url": "/static/js/2.9ad4d3d7.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/static/js/2.9ad4d3d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02fac6b4731bbe33ae2b",
+    "url": "/static/js/main.c85287ce.chunk.js"
+  },
+  {
+    "revision": "49cb89da10f542d4efa3",
+    "url": "/static/js/runtime-main.270950e9.js"
+  },
+  {
+    "revision": "fd58f500455946abaaa3787344dff06b",
+    "url": "/static/media/logo.fd58f500.jpg"
+  }
+]);
--- src/utils/makeRequest.tsx
+++ src/utils/makeRequest.tsx
@@ -5,7 +5,8 @@ import axios from 'axios';
 export default async(route: string, headers?: object) => new Promise((resolve, reject) => {
     axios({
         method: "GET",
-        url: "http://10.0.0.12:5000/" + route, //web route -> url: "http://localhost:5000/" + route,
+        //url: "http://10.0.0.12:5000/" + route, //web route -> url: "http://localhost:5000/" + route,
+        url: "http://69.55.55.121/" + route,
         headers: headers
     })
     .then(res => resolve(res))
